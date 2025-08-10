aefc020ac737417775b416268758090f6975c0f7
anandchirag7 <anandchirag7@gmail.com>
2025-08-10 10:25:06 +0530

    initial commit

---
+++ testing.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+user_db = {
+    1: {
+        "name": "John Doe",
+        "age": 30,
+            } ,
+    2: {
+        "name": "Jane Smith",
+        "age": 25
+    },   
+    3: {
+        "name": "Alice Johnson",
+        "age": 28,
+    },
+}
+
+class User(BaseModel):
+    name: str
+    age: int
+
+@app.put("/user_db/data/v1/update/{user_id}")
+def user_update(user_id: int, user_data: User):
+    if user_id in user_db:
+        user_db[user_id] = user_data.dict()
+        print(user_db)
+        return {"message": "User updated successfully", "user": user_db[user_id]}
+    else:
+        return {"error": "User not found"}
+
+@app.delete("/user_db/data/v1/delete/{user_id}")
+def delete_user(user_id: int):
+    if user_id in user_db:
+        del user_db[user_id]
+        print(user_db)
+        return {"message": "User deleted successfully", "user_id": user_id}
+    else:
+        return {"error": f"user {user_id} not found"}
+    
